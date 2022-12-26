1d94b1d9023d70dbf6086fedd2017063fae15edd
aslialpsoy <alpsoyasl@gmail.com>
2022-12-26 20:19:30 +0300

    First commit

---
+++ Assets/Scripts/Alien.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alien : MonoBehaviour
+{
+    public int scoreValue;
+    public GameObject explosion;
+
+    public void Kill()
+    {
+        UIManager.UpdateScore(scoreValue);
+        AlienMaster.allAliens.Remove(gameObject);
+        Instantiate(explosion, transform.position, Quaternion.identity);
+
+        if (AlienMaster.allAliens.Count == 0)
+        {
+            GameManager.SpawnNewWave();
+        }
+        
+        gameObject.SetActive(false);
+    }
+}
